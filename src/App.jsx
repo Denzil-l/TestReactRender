9b87c543d8f9ed99977f1c17f436e8506c8587d8
Denzil-l <Denzil.l@yandex.ru>
2023-08-19 23:45:04 +0300

    x

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Отправляем GET-запрос на "/test" при загрузке компоненты
+    fetch('/test')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Response from Server</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
+
+}
+
+
+export default App;
