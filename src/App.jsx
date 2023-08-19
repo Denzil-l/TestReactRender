7b65431871ffe41ea1fe00337ce5a0bdf4b41a37
Denzil-l <Denzil.l@yandex.ru>
2023-08-20 00:06:03 +0300

    x

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,25 @@ import { useState, useEffect } from "react";
 const App = () => {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    // Отправляем GET-запрос на "/test" при загрузке компоненты
-    fetch('/test')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  // useEffect(() => {
+  //   // Отправляем GET-запрос на "/test" при загрузке компоненты
+  //   fetch('/test')
+  //     .then(response => response.json())
+  //     .then(data => setData(data))
+  //     .catch(error => console.error('Error fetching data:', error));
+  // }, []);
 
   return (
-    <div className="App">
-      <h1>Response from Server</h1>
-      {data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      ) : (
-        <p>Loading data...</p>
-      )}
-    </div>
+    // <div className="App">
+    //   <h1>Response from Server</h1>
+    //   {data ? (
+    //     <pre>{JSON.stringify(data, null, 2)}</pre>
+    //   ) : (
+    //     <p>Loading data...</p>
+    //   )}
+    // </div>
+
+    <h2>Hi I am here</h2>
   );
 
 }
